9990943b6c4d02020ceadf275d0347584dc4ceb4
MrDan <56246704+MrDan17@users.noreply.github.com>
2019-10-07 16:42:19 +0700

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,41 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo 
+toilet -f big -F gay By Mr.Dan
+echo $red"Selamat Datang"
+echo
+sleep 2
+echo $yellow"Silahkan Pilih Tools Nya:"
+echo
+echo $cyan"1.) Stabilkan Jaringan"
+echo
+echo $blue"0.) Keluar Aja !!!"
+echo
+echo $white
+read -p "Pilihmana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Mr.Dan
+echo $green"Stabilkan Jaringan By Mr.Dan"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $green
+figlet "bye"
+echo $white"to be continued"
+sleep 3
+exit
+fi
